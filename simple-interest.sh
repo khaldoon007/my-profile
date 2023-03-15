f8fb41d4a3e77a93d60881a1ad44527dfdd22413
khaldoon007 <127400562+khaldoon007@users.noreply.github.com>
2023-03-16 01:06:26 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -6,8 +6,8 @@
 
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
-    khaldoon007
+   # khaldoon007
+  
    # Input:
    # p, principal amount
    # t, time period in years
